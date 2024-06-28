45203feff6922a0e0b1434b47a356155f4495eed
Thiago <tsr.souza@outlook.com>
2024-06-28 10:40:08 -0300

    Alterando pastas

--- 27-06-2024/sistema-aluno/script.js
+++ 27-06-2024/sistema-aluno/script.js
@@ -1,16 +1,30 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota");
 
+// Criando uma função para limpar o compo.
+function clearForm() {
+    inputNota.value = "";
+}
+
+
 // Criando uma função
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(inputNota.value);
+    const inputNotaValue = inputNota.value;
 
-    // Lógica
-    if(inputNota.value >= 6) {
-        alert("Você está aprovado");
-    } else {
-        alert("Você está reprovado");
+    // Validação de formulário
+    if (inputNotaValue === "") {
+        alert(`Preencha o campo obrigátorio.`)  ;
+        return false;
     }
 
+    // Lógica
+    if(inputNotaValue >= 6) {
+       alert(`A sua nota é ${inputNotaValue}, você está aprovado!`)
+    } else {
+        alert(`A sua nota é ${inputNotaValue}, você está reprovado!`)
+    }
+
+    // Chamando a função clearForm() para limpar o campo.
+    clearForm();
 });
